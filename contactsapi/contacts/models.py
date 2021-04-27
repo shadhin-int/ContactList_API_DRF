14ca39cfc034c03c7e967da7f2d2e71ee9c9988c
shadhin-int <shadhin.int@gmail.com>
2021-04-28 02:12:24 +0600

    created contacts model

--- contactsapi/contacts/models.py
+++ contactsapi/contacts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contacts(models.Model):
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    country_code = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    picture = models.URLField(null=True)
+    is_favourite = models.BooleanField(default=True)
